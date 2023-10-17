b465cd59d6c9efea9d0e77ed24d9860c63106b58
reece palmer <reecelpalmer@gmail.com>
2023-10-17 14:57:37 +0100

    Progress on 3 tests passing

--- shape.py
+++ shape.py
@@ -1,1 +1,15 @@
-pass  # YOUR CODE HERE
+class Shape:
+    def __init__(self, color, name):
+        self.color = color
+        self.name = name
+
+    def say_name(self):
+        return f"My name is {self.name}"
+
+class Rectangle(Shape):
+    def __init__(self, color, name, width, height):
+        super().__init__(color, name)
+
+class Circle(Shape):
+    def __init__(self, color, name, radius):
+        super().__init__(color, name)
