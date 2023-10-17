f9f87c02225ba3ce8c0d1cf2e47ab9f7bea8f5ca
reece palmer <reecelpalmer@gmail.com>
2023-10-17 15:03:51 +0100

    Progress on 7 tests passing

--- shape.py
+++ shape.py
@@ -9,9 +9,17 @@ class Shape:
 class Rectangle(Shape):
     def __init__(self, color, name, width, height):
         super().__init__(color, name)
+        self.width = width
+        self.height = height
 
     def say_name(self):
         return super().say_name() + " and I am a rectangle."
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
 
 
 class Circle(Shape):
