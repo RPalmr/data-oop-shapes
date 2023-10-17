6a5da7470f8c4a2028d1ae259877e2a6b214dd02
reece palmer <reecelpalmer@gmail.com>
2023-10-17 15:33:17 +0100

    Progress on 9 tests passing

--- shape.py
+++ shape.py
@@ -1,3 +1,11 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
+
+## Shape Challenge
+### Define PI
+PI = 3.14
+
+### Classes and Methods
+#### Shape Class
 class Shape:
     def __init__(self, color, name):
         self.color = color
@@ -6,25 +14,41 @@ class Shape:
     def say_name(self):
         return f"My name is {self.name}"
 
+#### Rectangle Class
 class Rectangle(Shape):
     def __init__(self, color, name, width, height):
         super().__init__(color, name)
         self.width = width
         self.height = height
+        self.name = name
 
     def say_name(self):
+        """Returns a string that says the name of the rectangle and its shape type."""
         return super().say_name() + " and I am a rectangle."
 
     def area(self):
+        """Calculates and returns the area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
+        """Calculates and returns the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
-
+#### Circle Class
 class Circle(Shape):
     def __init__(self, color, name, radius):
         super().__init__(color, name)
+        self.radius = radius
+        self.name = name
 
     def say_name(self):
+        """Returns a string that says the name of the circle and its shape type."""
         return super().say_name() + " and I am a circle."
+
+    def area(self):
+        """Calculates and returns the area of the circle."""
+        return round(PI * self.radius ** 2, 2)
+
+    def perimeter(self):
+        """Calculates and returns the perimeter of the circle."""
+        return round(2 * PI * self.radius, 2)
